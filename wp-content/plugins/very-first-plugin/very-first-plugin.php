196de07bd2f1678491e15a8c2646f1d26a77e5cb
Henry <kurg.henry@khk.ee>
2020-01-21 18:05:27 +0200

    modified plugin `Very First Plugin` version 1.0 (after activation of `Very First Plugin` version 1.0)

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Very First Plugin
+ * Plugin URI: http://kurghenry.ikt.khk.ee/
+ * Description: This is the very first plugin I ever created.
+ * Version: 1.0
+ * Author: Henry Kurg
+ * Author URI: http://kurghenry.ikt.khk.ee/
+ * */
