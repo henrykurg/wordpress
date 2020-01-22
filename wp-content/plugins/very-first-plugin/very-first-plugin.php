b9868c7c52dda86ce465cbc0a9678bc9c866f738
Henry <kurg.henry@khk.ee>
2020-01-22 08:46:34 +0200

    modified plugin `Very First Plugin` version 1.0

--- wp-content/plugins/very-first-plugin/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -7,3 +7,10 @@
  * Author: Henry Kurg
  * Author URI: http://kurghenry.ikt.khk.ee/
  * */
+ 
+ function nimed_suur_t2ht( $content ) {
+	$search  = array( 'Booking', 'Calendar', 'Appointment', 'Hour', 'WpDevArt' );
+	$replace = array( 'BOOKING', 'CALENDAR', 'APPOINTMENT', 'HOUR', 'WPDEVART' );
+	return str_replace( $search, $replace, $content );
+}
+add_filter( 'the_content', 'nimed_suur_t2ht' );
